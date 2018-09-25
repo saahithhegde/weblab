7c384a587abfed5a2ebc91114e59b7230a0fc9ef
saahithhegde <saahithhegde@yahoo.co.in>
2018-09-25 03:45:01 -0700

    Add files via upload

---
+++ prog7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<tile>WEB TECHNOLOGY LABARATORY[15CSL77]</title>
+	<meta http-equiv="refresh" content="1"/>
+	<style>
+		p{
+			color:black;
+			font-size:90px;
+			position:absolute;
+			top:50%;
+			left:50%;
+			transform:translate(-50%,-50%);
+		}
+	</style>
+	<p><?php echo date("h:i:s A");?></p>
+</head>
